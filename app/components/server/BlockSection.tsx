467cb8a18b9d46327cb6c8e871c8aecd4849b351
Timileyin <oluwayomitimileyin105@gmail.com>
2026-07-10 23:19:03 +0100

    minor border fix

--- app/components/server/BlockSection.tsx
+++ app/components/server/BlockSection.tsx
@@ -11,11 +11,11 @@ export interface BlockSectionProps {
 const BlockSection = ({ children, imageUrl, title }: BlockSectionProps) => {
       return (
             <div className="my-5">
-                  <div className='bg-[#e21893] flex flex-wrap border-[3.5px] rounded-md border-[#e21893]'>
+                  <div className='bg-[#e21893] flex flex-wrap border-3 rounded-md border-[#e21893]'>
                         <div className="w-[100%] sm:w-[50%]">
                               <FallbackImage height={1000} width={1000} className='w-[101%]' src={imageUrl} alt={title} />
                         </div>
-                        <div className="w-[100%] sm:w-[50%] flex justify-center flex-col text-center px-5 sm:px-12 text-[16px]! bg-white">
+                        <div className="w-[100%] sm:w-[50%] flex justify-center flex-col rounded-b-[3px] text-center px-5 sm:px-12 text-[16px]! bg-white">
                               {children}
                         </div>
                   </div >
